c0a64b64294152075ec61cf6a511f7923030c230
Charoline Margaretha Patty <160641534+charolinepatty@users.noreply.github.com>
2026-01-14 20:22:05 +0700

    first commit website ownerbajo rent car

---
+++ heroslide.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const slides = document.querySelectorAll(".hero-bg");
+  let current = 0;
+
+  setInterval(() => {
+    slides[current].classList.remove("active");
+    current = (current + 1) % slides.length;
+    slides[current].classList.add("active");
+  }, 3000);
+});
